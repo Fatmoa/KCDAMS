328b6111621f5474a74cf276d8c7c4462a7fb1df
Fatmoa <fatmamohd3654@gmail.com>
2024-05-10 22:10:04 +0300

    changes

---
+++ src/app/services/region.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegionService {
+  regionAPI = environment.baseUrl + "regions/"
+
+  constructor(private http:HttpClient) { }
+  getAllRegion(){
+    return this.http.get(this.regionAPI + "all")
+
+  }
+  addRegion(body:any){
+    return this.http.post(this.regionAPI + "save", body)
+
+  }
+
+  editRegion(id:any,body:any){
+    this.http.put(this.regionAPI + "editRegion"+id,body )
+  }
+
+}
