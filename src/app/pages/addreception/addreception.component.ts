fdbd2f4c4f416ac36b543d47166178ccbe06c6f9
Fatmoa <fatmamohd3654@gmail.com>
2024-05-30 15:45:29 +0300

    30-5-24 15:45

--- src/app/pages/addreception/addreception.component.ts
+++ src/app/pages/addreception/addreception.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatRippleModule} from '@angular/material/core';
 
 
 
@@ -17,34 +15,64 @@ export class AddreceptionComponent implements OnInit{
 
   radius: number | undefined;
 
-  combinedForm!:FormGroup;
+  TaarifaYaUsajili!:FormGroup;
+  MtoaTaarifa!:FormGroup;
+  Mawasiliano!:FormGroup
 
-
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required],
-  });
-
-
-
-
-
-  constructor(private _formBuilder: FormBuilder,private router: Router) {
-    // this.combinedForm = this._formBuilder.group({
-    //   firstCtrl: ['', Validators.required],
-    //   secondCtrl: ['', Validators.required],
-    //   thirdCtrl: ['', Validators.required],
-    // });
-  }
+  constructor(private _formBuilder: FormBuilder,private router: Router) {}
 
   ngOnInit(): void {
+    this.FormConfig()
+  }
+
+
+  FormConfig(){
+
+    this.TaarifaYaUsajili = new FormGroup({
+      firstName:new FormControl("",[Validators.required]),
+      secondName:new FormControl("",[Validators.required]),
+      lastName:new FormControl("",[Validators.required]),
+      dob:new FormControl("",[Validators.required]),
+      // age:new FormControl("",[Validators.required]),
+      // maritalStatus:new FormControl("",[Validators.required]),
+      // employment:new FormControl("",[Validators.required]),
+      phone:new FormControl("",[Validators.required]),
+      // children:new FormControl("",[Validators.required]),
+      // usajili:new FormControl("",[Validators.required]),
+      // address:new FormControl("",[Validators.required]),
+      Nida:new FormControl("",[Validators.required]),
+
+
+    })
+
+    this.MtoaTaarifa = new FormGroup({
+      ngoName:new FormControl("",[Validators.required]),
+      cowName:new FormControl("",[Validators.required]),
+      phoneNumber:new FormControl("",[Validators.required]),
+    })
+
+    this.Mawasiliano = new FormGroup({
+      garName1:new FormControl("",[Validators.required]),
+      relation1:new FormControl("",[Validators.required]),
+      // address1:new FormControl("",[Validators.required]),
+      pNum1:new FormControl("",[Validators.required]),
+      // garName2:new FormControl("",[Validators.required]),
+      // relation2:new FormControl("",[Validators.required]),
+      // address2:new FormControl("",[Validators.required]),
+      // pNum2:new FormControl("",[Validators.required]),
+
+    })
 
   }
+
+  SaveData(){
+    console.log(this.TaarifaYaUsajili.value)
+    console.log(this.MtoaTaarifa.value)
+    console.log(this.Mawasiliano.value)
+  }
+
+
+
   onBack(){
    this.router.navigateByUrl('receptions')
   }
