e4c537cca639ed561f983ff7da8fe962c419c06e
RASHID-KHAMIS <rashidkhmais168@gmail.com>
2024-08-03 01:38:39 +0300

    03/08 01:38

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  roleName:any;
+  constructor(private router:Router,
+    private route:ActivatedRoute
+  ){}
+  ngOnInit(): void {
+    this.roleName = sessionStorage.getItem('roleName');
+    
+   this.changeLog()
+  }
+
+  changeLog(){
+    let localData = sessionStorage.getItem("username");
+    if(localData == null){
+      this.router.navigateByUrl("/")
+    }
+  }
+
+  onLogOut(){
+    sessionStorage.clear();
+    this.router.navigateByUrl("/")
+  }
 
 }
